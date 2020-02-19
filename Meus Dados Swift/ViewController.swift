558de47e31304f65964ba5969078993418c21b32
Usuário Convidado <Guest@L2602MICRO24.local>
2020-02-19 20:43:53 -0300

    splash screen

--- Meus Dados Swift/ViewController.swift
+++ Meus Dados Swift/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelNome: UILabel!
+    
+    @IBOutlet weak var labelIdade: UILabel!
+    
+    @IBOutlet weak var labelCidade: UILabel!
+    
+    let initialNameState = "Meu nome é..."
+    let initialAgeState = "Minha idade é..."
+    let initialCityState = "Minha cidade é..."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelNome.text = initialNameState
+        labelIdade.text = initialAgeState
+        labelCidade.text = initialCityState
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func exibir(_ sender: Any) {
+        labelNome.text = "Lucas"
+        labelIdade.text = "23 anos"
+        labelCidade.text = "Cotia"
+    }
+    
+    
+    @IBAction func limpar(_ sender: Any) {
+        viewDidLoad()
+    }
 }
 
